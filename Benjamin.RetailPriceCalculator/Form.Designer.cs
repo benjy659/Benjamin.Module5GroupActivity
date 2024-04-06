f38d71078e8563f0add133926a3693496df8b99e
rrc_f2023_Benjamin <143534483+benjy659@users.noreply.github.com>
2024-04-06 14:43:19 -0500

    Add project files.

---
+++ Benjamin.RetailPriceCalculator/Form.Designer.cs
@@ -0,0 +1,83 @@
+﻿namespace Benjamin.RetailPriceCalculator
+{
+    partial class Form
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            ((System.ComponentModel.ISupportInitialize)(this.nudMarkupPercentage)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.errorProvider)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // txtWholesaleCost
+            // 
+            this.errorProvider.SetIconPadding(this.txtWholesaleCost, 3);
+            this.txtWholesaleCost.Text = "";
+            this.txtWholesaleCost.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            // 
+            // nudMarkupPercentage
+            // 
+            this.nudMarkupPercentage.Maximum = new decimal(new int[] {
+            100,
+            0,
+            0,
+            0});
+            this.nudMarkupPercentage.Minimum = new decimal(new int[] {
+            5,
+            0,
+            0,
+            0});
+            this.nudMarkupPercentage.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.nudMarkupPercentage.Value = new decimal(new int[] {
+            5,
+            0,
+            0,
+            0});
+            // 
+            // lblRetailPrice
+            // 
+            this.lblRetailPrice.Text = "";
+            // 
+            // Form
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.Control;
+            this.ClientSize = new System.Drawing.Size(481, 281);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MinimizeBox = false;
+            this.Name = "Form";
+            this.Text = "Retail Price Calculator";
+            ((System.ComponentModel.ISupportInitialize)(this.nudMarkupPercentage)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.errorProvider)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+    }
+}
---
+++ Benjamin.RetailPriceCalculator/Form.cs
@@ -0,0 +1,137 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Benjamin.RetailPriceCalculator
+{
+    public partial class Form : ADEV.ACE.RRC.Forms.ActivityFiveForm
+    {
+        public Form()
+        {
+            InitializeComponent();
+
+            this.txtWholesaleCost.TextChanged += TxtWholesaleCost_TextChanged;
+            this.nudMarkupPercentage.ValueChanged += NudMarkupPercentage_ValueChanged;
+            this.btnCalculate.Click += BtnCalculate_Click;
+            this.btnClear.Click += BtnClear_Click;
+            this.mnuFileExit.Click += MnuFileExit_Click;
+            this.mnuEditClear.Click += MnuEditClear_Click;
+            this.mnuToolsCalculate.Click += MnuToolsCalculate_Click;
+            
+        }
+
+        private void MnuToolsCalculate_Click(object sender, EventArgs e)
+        {
+           this.BtnCalculate_Click(sender, e);
+        }
+
+        /// <summary>
+        /// handles the clear evnt of the menu button clear
+        /// </summary>
+        private void MnuEditClear_Click(object sender, EventArgs e)
+        {
+            this.BtnClear_Click(sender, e);
+        }
+
+        /// <summary>
+        /// handles the menu exit event of the menu exit button
+        /// </summary>
+        private void MnuFileExit_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        /// <summary>
+        /// handle the clear event of the form
+        /// </summary>
+        private void BtnClear_Click(object sender, EventArgs e)
+        {
+            var result = MessageBox.Show("Do you want to clear the form?", "Clear Form", MessageBoxButtons.YesNo, MessageBoxIcon.Exclamation, MessageBoxDefaultButton.Button2);
+
+            if (result == DialogResult.Yes)
+            {
+                // Reset the form to its initial state
+                txtWholesaleCost.Text = "";
+                nudMarkupPercentage.Value = 5;
+                lblRetailPrice.Text = "";
+            }
+            else
+            {
+                txtWholesaleCost.Focus();
+            }
+        }
+
+        private void BtnCalculate_Click(object sender, EventArgs e)
+        {
+           
+            bool input = decimal.TryParse(txtWholesaleCost.Text, out decimal cost);
+
+            if (!input)
+            {
+                this.errorProvider.SetError(txtWholesaleCost, "The wholesale cost must be a numeric value");
+                this.txtWholesaleCost.Focus();
+                this.txtWholesaleCost.SelectAll();
+                return;
+            }
+            if (cost <= 0)
+            {
+                this.errorProvider.SetError(txtWholesaleCost, "The wholesale cost must be a value grater than zero");
+                this.txtWholesaleCost.Focus();
+                this.txtWholesaleCost.SelectAll();
+                return;
+
+            }
+            else
+            {
+                decimal markupPercentage = nudMarkupPercentage.Value;
+                decimal retailPrice = CalculateRetailPrice(cost, markupPercentage);
+                lblRetailPrice.Text = retailPrice.ToString("C");
+                this.txtWholesaleCost.Focus();
+            }
+            
+        }
+
+        
+        /// <summary>
+        /// handles when the markup percentage changes 
+        /// </summary>
+        private void NudMarkupPercentage_ValueChanged(object sender, EventArgs e)
+        {
+            var input = decimal.TryParse(txtWholesaleCost.Text, out decimal cost);
+            decimal markupPercentage = nudMarkupPercentage.Value;
+            decimal retailPrice = CalculateRetailPrice(cost, markupPercentage);
+            lblRetailPrice.Text = retailPrice.ToString("C");
+             
+
+        }
+
+        /// <summary>
+        /// method to calculate the retail price
+        /// </summary>
+        /// <param name="cost"></param>
+        /// <param name="markupPercentage"></param>
+        /// <returns></returns>
+        private decimal CalculateRetailPrice(decimal cost, decimal markupPercentage)
+        {
+            return cost * (1 + markupPercentage / 100);
+        }
+
+
+        /// <summary>
+        /// handles when the whoilesale cost textbox text changes.
+        /// </summary>
+        private void TxtWholesaleCost_TextChanged(object sender, EventArgs e)
+        {
+            this.lblRetailPrice.Text = string.Empty;
+            this.errorProvider.SetError(this.txtWholesaleCost, string.Empty);
+            this.txtWholesaleCost.Focus();
+            this.txtWholesaleCost.SelectAll();
+        }
+    }
+}
